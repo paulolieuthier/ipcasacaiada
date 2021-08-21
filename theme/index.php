d78e8962009d1064a57c279ac72a7dbefa6e1fe0
Paulo Lieuthier <paulolieuthier@gmail.com>
2021-08-21 09:15:52 -0300

    theme: set site title in theme's head tag

--- theme/index.php
+++ theme/index.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <title><?php bloginfo('name'); ?></title>
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" />
     <?php wp_head(); ?>
 </head>
-<body class="<?php echo join(' ', get_body_class()) ?>">
+<body class="<?php echo join(' ', get_body_class()); ?>">
     <div id="app"></div>
     <?php wp_footer(); ?>
 </body>
