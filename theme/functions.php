f4e2f3909771171e77c8acba0f0d6046e1f70870
Paulo Lieuthier <paulolieuthier@gmail.com>
2021-09-04 10:53:56 -0300

    theme: setup custom fields for theme data, setup composer project

--- theme/functions.php
+++ theme/functions.php
@@ -1,4 +1,125 @@
 <?php
+
+#
+# Add Theme Options in Admin Page
+#
+# WpGraphQLCrb (from matepaiva/wp-graphql-crb) registers the fields in WPGraphQL
+# Carbon_Fields (from htmlburger/carbon-fields) registers custom fields in WordPress
+#
+
+use WpGraphQLCrb\Container as WpGraphQLCrbContainer;
+use Carbon_Fields\Container;
+use Carbon_Fields\Field;
+
+add_action('carbon_fields_register_fields', function() {
+    WpGraphQLCrbContainer::register(
+        Container::make('theme_options', __('IPCC - Página Inicial', 'app'))
+            ->set_icon('dashicons-admin-home')
+            ->set_page_menu_position(4)
+            ->add_fields([
+                Field::make('separator', 'crb_separator_header', 'Cabeçalho'),
+                Field::make('html', 'crb_header_info')
+                    ->set_html('Título e Subtítulo devem ser definidos em Configurações Gerais.'),
+                Field::make('image', 'logo', 'Logo')
+                    ->set_value_type('url'),
+
+                Field::make('separator', 'crb_separator_banners', 'Banners'),
+                Field::make('complex', 'banners', '')
+                    ->add_fields([
+                        Field::make('image', 'image', 'Imagem')
+                            ->set_value_type('url'),
+                        Field::make('association', 'page', 'Página')
+                            ->set_types([[
+                                'type' => 'post',
+                                'post_type' => 'page',
+                            ]])
+                            ->set_min(1)
+                            ->set_max(1),
+                    ]),
+
+                Field::make('separator', 'crb_separator_welcome', 'Saudação'),
+                Field::make('text', 'welcome-title', 'Título')
+                    ->set_default_value('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'),
+                Field::make('text', 'welcome-subtitle', 'Subtítulo')
+                    ->set_default_value('Lorem ipsum dolor sit amet, consectetur adipiscing elit.'),
+                Field::make('image', 'welcome-video', 'Vídeo')
+                    ->set_type(['video'])
+                    ->set_value_type('url'),
+
+                Field::make('separator', 'crb_separator_about', 'Sobre Nós'),
+                Field::make('complex', 'about-us', '')
+                    ->add_fields([
+                        Field::make('text', 'title', 'Título'),
+                        Field::make('rich_text', 'content', 'Texto'),
+                        Field::make('image', 'image', 'Imagem')
+                            ->set_value_type('url'),
+                        Field::make('complex', 'actions', 'Botões')
+                            ->add_fields([
+                                Field::make('text', 'text', 'Texto'),
+                                Field::make('association', 'page', 'Página')
+                                    ->set_types([[
+                                        'type' => 'post',
+                                        'post_type' => 'page',
+                                    ]])
+                                    ->set_min(1)
+                                    ->set_max(1),
+                            ])
+                    ]),
+
+                Field::make('separator', 'crb_separator_groups', 'Ministérios'),
+                Field::make('complex', 'groups', '')
+                    ->add_fields([
+                        Field::make('text', 'name', 'Nome'),
+                        Field::make('image', 'image', 'Imagem')
+                            ->set_value_type('url'),
+                        Field::make('association', 'page', 'Página')
+                            ->set_types([[
+                                'type' => 'post',
+                                'post_type' => 'page',
+                            ]])
+                            ->set_min(1)
+                            ->set_max(1),
+                    ]),
+
+                Field::make('separator', 'crb_separator_contact', 'Contato'),
+                Field::make('text', 'contact-whatsapp', 'Whatsapp')
+                    ->set_default_value('(99) 99999-9999'),
+                Field::make('text', 'contact-phone', 'Telefone')
+                    ->set_default_value('(99) 99999-9999'),
+                Field::make('textarea', 'contact-location', 'Localização')
+                    ->set_rows(2)
+                    ->set_default_value(str_replace("<br />", "\n", 'Rua Sem Nome, S/N<br />Bairro, Cidade - UF')),
+                Field::make('text', 'contact-email', 'Email')
+                    ->set_default_value('email@email.com'),
+
+                Field::make('separator', 'crb_separator_footer', 'Rodapé'),
+                Field::make('text', 'footer-first-title', 'Rodapé 1 - Título')
+                    ->set_default_value('Título'),
+                Field::make('textarea', 'footer-first', 'Rodapé 1 - Texto')
+                    ->set_rows(3)
+                    ->set_default_value('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'),
+                Field::make('text', 'footer-second-title', 'Rodapé 2 - Título')
+                    ->set_default_value('Título'),
+                Field::make('textarea', 'footer-second', 'Rodapé 2')
+                    ->set_rows(3)
+                    ->set_default_value(str_replace("<br />", "\n", '09:00 – Culto Matutino<br />10:00 – Escola Dominical<br />18:30 – Culto Noturno')),
+                Field::make('text', 'social-youtube', 'Redes Sociais: Link do Youtube'),
+                Field::make('text', 'social-instagram', 'Redes Sociais: Link do Instagram'),
+                Field::make('text', 'social-facebook', 'Redes Sociais: Link do Facebook'),
+                Field::make('text', 'social-spotify', 'Redes Sociais: Link do Spotify'),
+            ])
+    );
+});
+
+add_action('after_setup_theme', function() {
+    require_once('vendor/autoload.php');
+    \Carbon_Fields\Carbon_Fields::boot();
+});
+
+#
+# Include generated script, style and custom javascript data in theme
+#
+
 add_action('wp_enqueue_scripts', function() {
     # insert css and javascript
     wp_enqueue_style('theme-css', get_template_directory_uri() . '/dist/index.css');
@@ -6,4 +127,5 @@ add_action('wp_enqueue_scripts', function() {
     # insert javascript variable with theme directory uri
     wp_localize_script('theme-js', 'wpParams', array('templateDir' => get_template_directory_uri()));
 });
+
 ?>
